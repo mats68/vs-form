afe145ceae7a732621b4d03baa367303f5255773
Matthias <mthaler@gmx.ch>
2017-12-16 14:45:42 +0100

    table mit inline edit

--- src/components/fields/vs-table.vue
+++ src/components/fields/vs-table.vue
@@ -1,10 +1,24 @@
 <template>
-  <v-data-table :headers="headers" :items="items" hide-actions class="elevation-1">
-    <template slot="items" slot-scope="props">
-      <td v-for="item in props.item" :key="item">{{ item }}</td>
-    </template>
+  <v-card>
+    <v-card-title>
+      <v-menu bottom offset-y lazy>
+        <v-btn flat icon color="indigo" slot="activator" @click="add">
+          <v-icon>add_box</v-icon>
+        </v-btn>
+        <!-- <vs-item :schema="schema" :designMode="designMode" node="plz"></vs-item> -->
+      </v-menu>
 
-  </v-data-table>
+      <v-spacer></v-spacer>
+      <v-text-field append-icon="search" label="Search" single-line hide-details v-model="search"></v-text-field>
+    </v-card-title>
+    <v-data-table :headers="headers" :items="items" :search="search" hide-actions class="elevation-1">
+      <template slot="items" slot-scope="props">
+        <!-- <td v-for="item in props.item" :key="item">{{ item }}</td> -->
+        <td v-for="item in props.item" :key="item"><v-text-field :slot="input" :value="item"></v-text-field></td>
+      </template>
+
+    </v-data-table>
+  </v-card>
 
 </template>
 
@@ -16,7 +30,14 @@ export default {
   data() {
     return {
       headers: [],
-      items: []
+      items: [],
+      search: ''
+    }
+  },
+  methods: {
+    add() {
+      this.items.push({ [this.item.field]: '' })
+      // todo  update value this.editValue.push('')
     }
   },
   created() {
@@ -24,6 +45,7 @@ export default {
     this.items = this.editValue.map(val => {
       return { [this.item.field]: val }
     })
+    // console.log(this.schema)
   }
 }
 </script>
