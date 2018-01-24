2b04b54434e48b75cf360db374b904515f1cdcfd
Matthias <mthaler@gmx.ch>
2018-01-24 10:28:34 +0100

    expansionpanel

---
+++ src/components/containers/vs-expansion-panel.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-expansion-panel>
+    <slot></slot>
+  </v-expansion-panel>
+</template>
+
+<script>
+import mixin from '../vs-item-mixin'
+
+export default {
+  mixins: [mixin]
+}
+</script>
+
+<style>
+
+</style>
