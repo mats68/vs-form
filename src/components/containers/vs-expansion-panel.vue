e9c9cf544739c8974402abca3dd02b7f773f7c53
Matthias <mthaler@gmx.ch>
2018-01-31 21:42:54 +0100

    can set active Tab in schema

--- src/components/containers/vs-expansion-panel.vue
+++ src/components/containers/vs-expansion-panel.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-expansion-panel>
-    <v-expansion-panel-content v-for="component in getChildrenList" :key="component.id">
-      <div slot="header">{{component.label}}</div>
+  <v-expansion-panel v-bind="fieldProperties">
+    <v-expansion-panel-content v-for="component in getChildrenList" :key="component.id" :value="component.id === compo.activeTab">
+      <div slot="header" @click="tabOpen(component.id)">{{component.label}}</div>
       <vs-item v-bind="currentProperties(schema,component.node)" v-on:updateValue="updateValue"></vs-item>
     </v-expansion-panel-content>
   </v-expansion-panel>
@@ -12,6 +12,14 @@ import mixin from '../vs-item-mixin'
 
 export default {
   mixins: [mixin],
+  computed: {
+  },
+  methods: {
+    tabOpen(id) {
+      this.compo.activeTab = id
+      // alert(id)
+    }
+  }
 }
 </script>
 
